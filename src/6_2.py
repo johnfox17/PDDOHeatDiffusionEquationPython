0366a3d04d4335bb8f291965d1a33417b7b9e7da
johnfox17 <johnfox17@gmail.com>
2022-07-18 22:57:17 -0700

    Created function to extract coordiantes from PDGeo numpy array and made many variables global

--- src/6_2.py
+++ src/6_2.py
@@ -1,9 +1,27 @@
-#import csv
-#import numpy as np
+import numpy as np
+import sklearn
+import math
+
 
 inputsPath = "../inputs/"
 
+def extractCoordinates():
+    global coordinates
+    coordinates = []
+    if aType == 0 :
+        for i in range(totalNodes):
+            aux = [PDGeo[i][0], PDGeo[i][1], PDGeo[i][2], PDGeo[i][3], PDGeo[i][4]]
+            coordinates.append(aux)    
+    else:
+        for i in range(totalNodes):
+            aux = [PDGeo[i][0], PDGeo[i][1], PDGeo[i][2], PDGeo[i][3]]
+            coordinates.append(aux)
+    coordinates = np.array(coordinates)
+
 def loadPDGeoInput():
+
+    global totalNodes, PDGeo
+
     PDGeoInput = str(inputsPath + "PDgeom3D.dat")
     with open(PDGeoInput, newline='\n') as fp:
         PDGeo = []
@@ -11,22 +29,28 @@ def loadPDGeoInput():
         for i in range(totalNodes):
             line = list(map(float,fp.readline().split()))
             PDGeo.append(line)
+    PDGeo = np.array(PDGeo)
+    extractCoordinates()
 
 def inputForPDDO():
+    
+    global PDGeoFileName, aType, morder, n1order, n2order, n3order, timeFlag, fType
+    global delx, dely, delz, numDiffOps, numberDiffOperators, numBC, BC, nout
+
     PDoprInput = inputsPath + "PDopr.inp"
     with open(PDoprInput) as fp:
         line = fp.readline() #file name
         PDGeoFileName = fp.readline() #file name
         line = fp.readline() #atype, morder, n1order, n2order, n3order, timeFlag, ftype
-        atype = fp.readline().split()
-        morder = int(atype[0])
-        n1order = int(atype[1])
-        n2order = int(atype[2])
-        n3order = int(atype[3])
-        timeFlag = int(atype[4])
-        ftype = int(atype[5])
+        line = fp.readline().split()
+        aType = int(line[0])
+        morder = int(line[1])
+        n1order = int(line[2])
+        n2order = int(line[3])
+        n3order = int(line[4])
+        timeFlag = int(line[5])
         line = fp.readline() #family type
-        familyType = int(fp.readline())
+        fType = int(fp.readline())
         line = fp.readline() #delx dely delz
         deltas = fp.readline().split()
         delx = float(deltas[0].replace("d","e"))
@@ -56,7 +80,6 @@ def inputForPDDO():
                 BC.append(aux) 
         line = fp.readline() #nout
         nout = int(fp.readline())
-        nout = 2
         if nout>0:
             line = fp.readline() #n1
             numOut = []
@@ -65,11 +88,10 @@ def inputForPDDO():
                 numOut.append(list(map(int,line)))
         loadPDGeoInput()
 
-
 def main():
     
     inputForPDDO()
-
+    print(coordinates)
     #a = input('').split(" ")[0]
     #PDGeomPath = '../data/PDgeom3D.dat'
     #[nodeNum, PDGeo] = loadPDGeom3D(PDGeomPath) 

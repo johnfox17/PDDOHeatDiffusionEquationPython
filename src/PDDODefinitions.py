dc88bed0f9a4027773c638b3f780b7c501d87e7e
johnfox17 <johnfox17@gmail.com>
2022-07-20 21:28:53 -0700

    Added PDDODefinitions.py where I constructed a class with the PDDO information

---
+++ src/PDDODefinitions.py
@@ -0,0 +1,21 @@
+
+class PDDOOperator:
+    def __init__(self):
+         self.numDiffOps = 0
+         self.norder = 0
+         self.n1order = 0
+         self.n2order = 0
+         self.n3order = 0
+         self.nsize = 0
+         self.nskip = 0
+         self.morder = 0
+         self.asymFlag = False
+         self.numBC = 0
+         self.BC = 0
+         self.nteqs = 0
+         self.ncons = 0
+         self.aType = 0
+         self.nout = 0
+         self.numOut = 0
+         self.nwk = 0
+

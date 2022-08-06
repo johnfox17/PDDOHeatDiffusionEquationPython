ebd424d1aa609fa8b8c74c27534fffaff6f613e0
John <johnfox17@gmail.com>
2022-08-05 18:00:56 -0700

    Cleaned 6_2.py a bit

--- src/6_2.py
+++ src/6_2.py
@@ -26,11 +26,6 @@ def main():
     Geometry = geometry.extractCoordinates(PDGeo,totalNodes, PDDOOperator.aType)
     #creating node families
     Geometry.nodeFamiliesIdx = geometry.generateNodeFamilies(Geometry)
-    
-    for i in range(1000):
-        print(Geometry.nodeFamiliesIdx[i])
-        a = input('').split(" ")[0]
-
     #Extract Boundaries
     #Geometry.boundaries = geometry.extractBoundaries(PDDOOperator, Geometry)
     #get size of system of equations

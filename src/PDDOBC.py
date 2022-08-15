9108303d039b4e010bf1a01dd62a425da38bda33
John <johnfox17@gmail.com>
2022-08-14 17:20:08 -0700

    Created and started implementing file PDDOBC

---
+++ src/PDDOBC.py
@@ -0,0 +1,16 @@
+import math
+import numpy as np
+import scipy
+
+class PDDOBC:
+    def __init__(self, pDDOOperator,geometry,diffEquation):
+        self.SpSysMatBC = SetupODEBC2D()
+        self.SysVecBC = SetupODEVecBC2D()
+
+
+
+def SetupODEBC2D():
+    return 0
+
+def SetupODEVecBC2D():
+    return 0 
